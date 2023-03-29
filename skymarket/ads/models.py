3c9a53c2620fb56a56a1f80b9d9028f7b2370581
macbook <mypur86@yahoo.com>
2023-03-29 13:30:17 +0300

    Initial

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -3,10 +3,24 @@ from django.db import models
 
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    class Meta:
+        verbose_name = "Обьявление"
+        verbose_name_plural = "Обьявления"
+
+    image = models.ImageField(upload_to='ad_pics/', default=None, blank=True)
+    title = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=1000, blank=True, null=True, default=None)
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+
+    text = models.TextField()
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    ad = models.ForeignKey("ads.Ad", on_delete=models.CASCADE)
--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -1,16 +1,22 @@
-from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
-from users.managers import UserManager
 from phonenumber_field.modelfields import PhoneNumberField
-from django.utils.translation import gettext_lazy as _
 
 
-class UserRoles:
-    # TODO закончите enum-класс для пользователя
-    pass
+class UserRoles(models.TextChoices):
+    ADMIN = 'admin'
+    MEMBER = 'member'
 
 
 class User(AbstractBaseUser):
-    # TODO переопределение пользователя.
-    # TODO подробности также можно поискать в рекоммендациях к проекту
-    pass
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True, null=True)
+    phone = PhoneNumberField()
+    role = models.CharField(max_length=30, choices=UserRoles.choices, default=UserRoles.MEMBER)
+    is_active = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='user_pics/', default=None, blank=True)
